490c8d7aaf98e4e0a14953ad2b0d4fd35549604e
Anass-Zar <anasszarioh35@gmail.com>
2024-10-24 14:39:28 +0100

    ...

--- utils/mail.utils.js
+++ utils/mail.utils.js
@@ -1,26 +1,26 @@
 import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 const transport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
-  secure: process.env.NODE_ENV !== 'development',
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASSWORD,
-  },
+    pass: process.env.MAIL_PASSWORD
+  }
 });
 
 export const sendEmail = async (dto) => {
-  const { sender, recipients, subject, htmlMessage } = dto;
-
-  // Generate a plain text version for the email
-  const textMessage = htmlMessage.replace(/<\/?[^>]+(>|$)/g, "");
+  const { sender, receipients, subject, message } = dto;
 
   return await transport.sendMail({
     from: sender,
-    to: recipients,
+    to: receipients,
     subject,
-    html: htmlMessage,
-    text: textMessage,
+    html: message,
+    text: message,
   });
+
 };
