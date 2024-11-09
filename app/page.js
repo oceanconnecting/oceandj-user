53f56a606fa52205b7b0b04c4756014a23a3a9fd
Anass-Zar <anasszarioh35@gmail.com>
2024-11-09 12:36:11 +0100

    ...

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Companies } from "@/components/home/companies";
 import { Types } from "@/components/home/types";
 import { Slide } from "@/components/home/slide";
 import { Discount } from "@/components/home/discount";
-// import { LastNews } from "@/components/home/last-news";
+import { LastNews } from "@/components/home/last-news";
 import { Features } from "@/components/home/features";
 import { News } from "@/components/home/news";
 import { Deal } from "@/components/home/deal";
